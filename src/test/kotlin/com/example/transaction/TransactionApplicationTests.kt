a6b19da66688e9242b508486ffda28997a797f93
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2023-10-22 15:36:57 +0900

    add test code

--- src/test/kotlin/com/example/transaction/TransactionApplicationTests.kt
+++ src/test/kotlin/com/example/transaction/TransactionApplicationTests.kt
@@ -1,13 +1,23 @@
 package com.example.transaction
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class TransactionApplicationTests {
 
+	@Autowired
+	lateinit var producer: CustomProducer
+
 	@Test
-	fun contextLoads() {
+	fun something() {
+		producer.send(CustomProducer.UserInfo("홍길동", "010-1234-5678", "홍홍"))
+	}
+
+	@Test
+	fun something2() {
+		producer.send(CustomProducer.OtherUserInfo("홍길동", 12))
 	}
 
 }
