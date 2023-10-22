cef9427662d6359f9fec8c386f4dd93076dca631
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2023-10-22 15:32:49 +0900

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,12 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java:8.0.33")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 }
